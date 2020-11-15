c93e517770b25fb1e41a49504c2d465f5c73a3eb
kowalix-pl <kowalix007@gmail.com>
2020-11-15 19:31:50 +0000

    change homepage route to languages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :languages
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    root to: "home#index"
-  get "home", action: :index, controller: 'home'
+    root to: "languages#index"
+  
 end
