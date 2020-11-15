9ea051ea12aeede8de0132f6055bbaab44fcd35d
kowalix-pl <kowalix007@gmail.com>
2020-11-15 19:05:58 +0000

    complete language scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_14_201053) do
+ActiveRecord::Schema.define(version: 2020_11_15_185335) do
+
+  create_table "languages", force: :cascade do |t|
+    t.string "name"
+    t.string "language_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
