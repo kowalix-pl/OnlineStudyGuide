070ec2d258c6c74142056b6bd5a36263005dc329
kowalix-pl <kowalix007@gmail.com>
2020-11-16 18:54:39 +0000

    CRUD for code_snippets

--- app/controllers/code_snippets_controller.rb
+++ app/controllers/code_snippets_controller.rb
@@ -1,10 +1,11 @@
 class CodeSnippetsController < ApplicationController
+  before_action :set_language
   before_action :set_code_snippet, only: [:show, :edit, :update, :destroy]
 
   # GET /code_snippets
   # GET /code_snippets.json
   def index
-    @code_snippets = CodeSnippet.all
+    @code_snippets = @language.code_snippets
   end
 
   # GET /code_snippets/1
@@ -14,7 +15,7 @@ class CodeSnippetsController < ApplicationController
 
   # GET /code_snippets/new
   def new
-    @code_snippet = CodeSnippet.new
+    @code_snippet = @language.code_snippets.new
   end
 
   # GET /code_snippets/1/edit
@@ -24,11 +25,11 @@ class CodeSnippetsController < ApplicationController
   # POST /code_snippets
   # POST /code_snippets.json
   def create
-    @code_snippet = CodeSnippet.new(code_snippet_params)
+    @code_snippet = @language.code_snippets.new(code_snippet_params)
 
     respond_to do |format|
       if @code_snippet.save
-        format.html { redirect_to @code_snippet, notice: 'Code snippet was successfully created.' }
+        format.html { redirect_to [@language,@code_snippet], notice: 'Code snippet was successfully created.' }
         format.json { render :show, status: :created, location: @code_snippet }
       else
         format.html { render :new }
@@ -42,7 +43,7 @@ class CodeSnippetsController < ApplicationController
   def update
     respond_to do |format|
       if @code_snippet.update(code_snippet_params)
-        format.html { redirect_to @code_snippet, notice: 'Code snippet was successfully updated.' }
+        format.html { redirect_to [@language,@code_snippet], notice: 'Code snippet was successfully updated.' }
         format.json { render :show, status: :ok, location: @code_snippet }
       else
         format.html { render :edit }
@@ -56,7 +57,7 @@ class CodeSnippetsController < ApplicationController
   def destroy
     @code_snippet.destroy
     respond_to do |format|
-      format.html { redirect_to code_snippets_url, notice: 'Code snippet was successfully destroyed.' }
+      format.html { redirect_to language_code_snippets_url(@language), notice: 'Code snippet was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -64,11 +65,17 @@ class CodeSnippetsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_code_snippet
-      @code_snippet = CodeSnippet.find(params[:id])
+      @code_snippet = @language.code_snippets.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def code_snippet_params
       params.require(:code_snippet).permit(:code)
     end
+
+   # Use callbacks to share common setup or constraints between actions.
+    def set_language
+      @language = current_user.languages.find(params[:language_id])
+    end
+
 end
