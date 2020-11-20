c17104af461cc17bdc2795a59b02327e5a904a01
kowalix-pl <kowalix007@gmail.com>
2020-11-20 19:27:47 +0000

    create student certification page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable       
   has_many :languages 
   has_many :user_certifications
   has_many :certifications, through: :user_certifications
   has_many :code_snippets, through: :languages      
   has_many :learning_resources, through: :languages      
+
+  def self.students
+    where(lecturer: false)  
+  end 
 
 end
